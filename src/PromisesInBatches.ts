ab79f4d5f9e36318fafee8914b84cc44e824f59c
morishin <sntr92@gmail.com>
2019-02-13 01:31:11 +0900

    Fix initial implementation

---
+++ src/PromisesInBatches.ts
@@ -0,0 +1,22 @@
+export async function makePromiseBatch<T>(
+  promiseGenerators: (() => Promise<T>)[],
+  batchSize: number
+): Promise<T[][]> {
+  const chunkedPromiseGenerators = chunk(promiseGenerators, batchSize);
+  var results = [];
+  for (const generators of chunkedPromiseGenerators) {
+    const result = await Promise.all(generators.map(g => g()));
+    results.push(result);
+  }
+  return results;
+}
+
+function chunk<T>(array: T[], chunkSize: number): T[][] {
+  return [...Array(Math.floor(array.length / chunkSize)).keys()]
+    .map(v => v * chunkSize)
+    .map(v => array.slice(v, v + chunkSize))
+    .concat([
+      array.slice(array.length - (array.length % chunkSize), array.length)
+    ])
+    .filter(a => a.length > 0);
+}
